b96067ee102c50a6a5b569b171839d68e8a47b28
63BITS <mike@63bits.com>
2025-11-24 09:41:55 +0400

    Products models breakdown to files

--- SixtyThreeBits.Web/Models/Website/Products/ProductsModel.cs
+++ SixtyThreeBits.Web/Models/Website/Products/ProductsModel.cs
@@ -54,41 +54,4 @@ namespace SixtyThreeBits.Web.Models.Website
         }
         #endregion
     }
-
-    public class ProductModel : ModelBase
-    {
-        #region Methods
-        public async Task<ViewModel> GetViewModel(int? productID)
-        {
-            var viewModel = default(ViewModel);
-
-            var repository = RepositoriesFactory.CreateProductsRepository();
-            var product = await repository.ProductsGetSingleByID(productID);
-            if(product != null)
-            {
-                viewModel = new ViewModel();
-                viewModel.PageTitle = product.ProductName;
-                viewModel.ProductName = product.ProductName;
-                viewModel.ProductCoverImageHttpPath = FileStorage.GetUploadedFileHttpPath(product.ProductCoverImageFilename);
-                viewModel.ProductPrice = Utilities.FormatPrice(price: product.ProductPrice, currencySign: "$");
-
-                PageTitle.Set(viewModel.PageTitle);
-            }
-
-            return viewModel;
-        }
-        #endregion
-
-        #region Nested Classes
-        public class ViewModel
-        {
-            #region Properties
-            public string PageTitle { get; set; }
-            public string ProductName { get; set; }
-            public string ProductCoverImageHttpPath { get; set; }
-            public string ProductPrice { get; set; }
-            #endregion            
-        }
-        #endregion
-    }
 }
